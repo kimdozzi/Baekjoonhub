a4e664f76b141f212c3ff78f071605b7af740c59
DoHyung Kim <neo5188@gmail.com>
2023-12-17 18:00:34 +0900

    [Silver II] Title: 아카라카, Time: 144 ms, Memory: 120288 KB -BaekjoonHub

--- 백준/Silver/23304. 아카라카/아카라카.py
+++ 백준/Silver/23304. 아카라카/아카라카.py
@@ -1,42 +1,17 @@
-import sys,copy
+import sys
 si = sys.stdin.readline
 
-s=si().rstrip()
+def aka(s):
+    n = len(s)
+    if n == 1: return True
 
-if len(s) <= 3 :
-    for i in range(len(s)) :
-        if s[i] != s[len(s)-i-1] :
-            print("IPSELENTI")
-    print("AKARAKA")
-    exit(0)
+    xa, xb = s[:n//2], s[n-(n//2):]
 
-def is_palindrome(s) :
-    half_length = len(s) // 2
-    if len(s) % 2 == 0 :
-        prefix = s[:half_length]
-        suffix = s[half_length:]
-    else :
-        prefix = s[:half_length]
-        suffix = s[half_length + 1:]
+    if xa == xb: return aka(xa)
+    else: return False
 
-    if half_length == 2 :
-        if prefix == suffix[::-1]:
-            print("AKARAKA")
-        else:
-            print("IPSELENTI")
-        exit(0)
-
-    if half_length > 1 :
-        is_palindrome(prefix)
-        is_palindrome(suffix)
-    else :
-        if prefix == suffix[::-1] :
-            return
-        else :
-            print("IPSELENTI")
-            exit(0)
-
-    print("AKARAKA")
-    exit(0)
-
-is_palindrome(s)
+a = si().rstrip()
+if aka(a):
+    print('AKARAKA')
+else:
+    print('IPSELENTI')
