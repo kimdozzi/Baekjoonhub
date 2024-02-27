76d53d3b3f778799880a0ca075405216a016af8d
DoHyung Kim <neo5188@gmail.com>
2024-02-27 23:41:40 +0900

    [Silver IV] Title: 좋은 구간, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1059. 좋은 구간/좋은 구간.py
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.py
@@ -6,26 +6,21 @@ arr = list(map(int, si().split()))
 arr.sort()
 n = int(si())
 
-if n in arr or arr[-1] < n:
+x,y = 0,1001
+cnt = 0
+
+if arr[-1] < n or n in arr:
     print(0)
     exit(0)
 
-x,y=0,1001
-for i in range(len(arr)) :
-    if i == 0 and arr[i] > n :
-        y = arr[i]
-        break
-
+for i in range(l) :
     if arr[i] > n :
-        x = arr[i-1]
+        if i != 0 :
+            x = arr[i-1]
         y = arr[i]
         break
 
-cnt = 0
-flag = False
 for i in range(x+1, n+1) :
-    if flag :
-        break
     for j in range(n, y) :
         if i == j : continue
         cnt += 1
