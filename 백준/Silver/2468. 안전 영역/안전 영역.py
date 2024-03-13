0d3190f781557d29efa329f3693097267d93092b
DoHyung Kim <neo5188@gmail.com>
2024-03-13 20:29:32 +0900

    [Silver I] Title: 안전 영역, Time: 260 ms, Memory: 117184 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,41 +1,62 @@
+import sys
 from collections import deque
+si = sys.stdin.readline
 
-n = int(input())
-graph = []
-max_height = 0
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
-    max_height = max(max_height, max(graph[-1]))
-    
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-    
-def bfs(x, y, r):
-    queue = deque()
-    queue.append((x, y))
-    visited[x][y] = 1
-    
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx<0 or nx>=n or ny<0 or ny>=n:
-                continue
-            if visited[nx][ny] == 1 or graph[nx][ny] <= r:
-                continue
-            queue.append((nx, ny))
-            visited[nx][ny] = 1
+def get_max_water(board: list) -> int:
+    temp = -1
+    for i in board:
+        temp = max(temp, max(i))
+    return temp
 
-result = 0
-for i in range(max_height):
-    count = 0
-    visited = [[0]*n for _ in range(n)]
-    for j in range(n):
-        for k in range(n):
-            if graph[j][k] > i and visited[j][k] == 0:
-                bfs(j, k, i)
-                count += 1
-    result = max(result, count)
-    
-print(result)
+
+
+def bfs(i: int, j: int, water_depth: int, visited: list, n: int, board:list):
+    dirs = [[0,1],[0,-1],[1,0],[-1,0]]
+    q = deque()
+    q.append((i,j))
+    visited[i][j] = True
+
+    while q :
+        x,y = q.popleft()
+        for i in range(4) :
+            nx = x + dirs[i][0]
+            ny = y + dirs[i][1]
+            if not (0 <= nx < n and 0 <= ny < n) : continue
+            if visited[nx][ny] or board[nx][ny] <= water_depth : continue
+            q.append((nx, ny))
+            visited[nx][ny] = True
+
+    return True
+
+
+def solve(water_depth: int, board: list, n: int) -> int:
+    visited = [[False] * n for _ in range(n)]
+    cnt = 0
+    for i in range(n) :
+        for j in range(n) :
+            if board[i][j] <= water_depth :
+                visited[i][j] = True
+
+    for i in range(n) :
+        for j in range(n) :
+            if not visited[i][j] and board[i][j] > water_depth :
+                if bfs(i,j,water_depth,visited, n, board) :
+                    cnt += 1
+    return cnt
+
+
+def main() :
+    n = int(si()) # max 100
+    board = [list(map(int, si().split())) for _ in range(n)]
+    ans = 0
+    max_water = get_max_water(board) # max 100
+
+
+    for i in range(max_water) :
+        ans = max(ans, solve(i, board, n))
+
+    print(ans)
+
+if __name__ == "__main__" :
+    main()
+
