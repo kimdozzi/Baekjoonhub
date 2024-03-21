196c88972d9b571de1a196ea8475ec642d9f34a9
DoHyung Kim <neo5188@gmail.com>
2024-03-21 20:23:16 +0900

    [Silver II] Title: DNA 비밀번호, Time: 368 ms, Memory: 164084 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -1,20 +1,4 @@
 import sys
-from collections import defaultdict
-si = sys.stdin.readline
-s,p=map(int,si().split())
-strList=list(si().rstrip())
-arr = list(map(int, si().split()))
-dic={'A':0, 'C':0, 'G':0, 'T':0}
-
-arr_dic = defaultdict(int)
-idx = 0
-for i in ['A','C','G','T'] :
-    arr_dic[i] += arr[idx]
-    idx+=1
-
-for i in strList[:p] :
-    dic[i] += 1
-cnt = 0
 
 def check() :
     global arr_dic, dic
@@ -25,10 +9,28 @@ def check() :
                 temp += 1
     return True if temp==4 else False
 
+si = sys.stdin.readline
+s,p=map(int,si().split())
+strList=list(si().rstrip())
+arr = list(map(int, si().split()))
+
+dic={'A':0, 'C':0, 'G':0, 'T':0}
+idx,cnt = 0,0
+arr_dic = dict()
+
+for i in ['A','C','G','T'] :
+    if i not in arr_dic :
+        arr_dic[i] = 0
+
+    arr_dic[i] += arr[idx]
+    idx+=1
+
+for i in strList[:p] :
+    dic[i] += 1
+
 cnt = 1 if check() else 0
 
-for i in range(s-p+1) :
-    if i == 0 : continue
+for i in range(1,s-p+1) :
     dic[strList[i-1]] -= 1
     dic[strList[i+p-1]] += 1
     if check() :
