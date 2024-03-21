1dac837a10d913c12238e633d8ffdf4c03b6a66f
DoHyung Kim <neo5188@gmail.com>
2024-03-21 20:20:06 +0900

    [Silver II] Title: DNA 비밀번호, Time: 384 ms, Memory: 167488 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -4,10 +4,7 @@ si = sys.stdin.readline
 s,p=map(int,si().split())
 strList=list(si().rstrip())
 arr = list(map(int, si().split()))
-
-if s < p :
-    print(0)
-    exit(0)
+dic={'A':0, 'C':0, 'G':0, 'T':0}
 
 arr_dic = defaultdict(int)
 idx = 0
@@ -15,18 +12,9 @@ for i in ['A','C','G','T'] :
     arr_dic[i] += arr[idx]
     idx+=1
 
-dic={'A':0, 'C':0, 'G':0, 'T':0}
 for i in strList[:p] :
     dic[i] += 1
 cnt = 0
-for k,v in arr_dic.items() :
-    if k in dic :
-        if dic[k] >= arr_dic[k] :
-            cnt += 1
-
-cnt = 1 if cnt==4 else 0
-
-# print(arr_dic, dic, cnt)
 
 def check() :
     global arr_dic, dic
@@ -37,6 +25,8 @@ def check() :
                 temp += 1
     return True if temp==4 else False
 
+cnt = 1 if check() else 0
+
 for i in range(s-p+1) :
     if i == 0 : continue
     dic[strList[i-1]] -= 1
@@ -45,6 +35,3 @@ for i in range(s-p+1) :
         cnt+=1
 
 print(cnt)
-
-
-
