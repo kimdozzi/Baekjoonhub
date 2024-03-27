a233a2c2434206a61ccfbbad42c6a64839a3b997
DoHyung Kim <neo5188@gmail.com>
2024-03-27 11:36:27 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 670.51 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/README.md
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/README.md
@@ -1,10 +1,10 @@
 # [level 3] 파괴되지 않은 건물 - 92344 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92344#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92344) 
 
 ### 성능 요약
 
-메모리: 132 MB, 시간: 700.14 ms
+메모리: 132 MB, 시간: 670.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 27일 10:52:26
+2024년 03월 27일 11:36:25
 
 ### 문제 설명
 
--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,34 +1,36 @@
 def solution(board, skill):
     r = len(board)
     c = len(board[0])
-    
-    prefix_sum = [[0] * (c+1) for _ in range(r+1)]
-    
+
+    prefix_sum = [[0] * (c+2) for _ in range(r+2)]
+
     for i in range(len(skill)) :
         ty, r1,c1,r2,c2,degree = skill[i]
+        r1 +=1; r2+=1; c1+=1; c2+=1
         if ty == 1 :
-            prefix_sum[r1][c1] += degree
-            prefix_sum[r2+1][c2+1] += degree
-            prefix_sum[r1][c2+1] -= degree
-            prefix_sum[r2+1][c1] -= degree
-        else :
-            prefix_sum[r1][c1] -= degree
-            prefix_sum[r2+1][c2+1] -= degree
-            prefix_sum[r1][c2+1] += degree
-            prefix_sum[r2+1][c1] += degree
-            
-    for i in range(len(prefix_sum)) :
-        for j in range(1,len(prefix_sum[i])) :
-            prefix_sum[i][j] = prefix_sum[i][j-1] + prefix_sum[i][j]
+            degree *= -1
+        prefix_sum[r1][c1] += degree
+        prefix_sum[r2+1][c2+1] += degree
+        prefix_sum[r1][c2+1] -= degree
+        prefix_sum[r2+1][c1] -= degree
+        
+
+#     for i in range(len(prefix_sum)) :
+#         for j in range(1,len(prefix_sum[i])) :
+#             prefix_sum[i][j] = prefix_sum[i][j-1] + prefix_sum[i][j]
+
+#     for j in range(len(prefix_sum[0])) :
+#         for i in range(1,len(prefix_sum)) :
+#             prefix_sum[i][j] = prefix_sum[i-1][j] + prefix_sum[i][j]
     
-    for j in range(len(prefix_sum[0])) :
-        for i in range(1,len(prefix_sum)) :
-            prefix_sum[i][j] = prefix_sum[i-1][j] + prefix_sum[i][j]
-    
+    for i in range(1,r+1) :
+        for j in range(1,c+1):
+            prefix_sum[i][j] += prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1]
+
     answer = 0
-    for i in range(len(board)) :
-        for j in range(len(board[i])) :
-            board[i][j] -= prefix_sum[i][j]
+    for i in range(r) :
+        for j in range(c) :
+            board[i][j] += prefix_sum[i+1][j+1]
             if board[i][j] > 0 : answer += 1
-    
+
     return answer
