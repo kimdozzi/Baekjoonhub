74d76293d9589704a881a487de70b771d3460f8e
DoHyung Kim <neo5188@gmail.com>
2024-04-09 18:47:14 +0900

    [Gold IV] Title: 공유기 설치, Time: 188 ms, Memory: 112084 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -0,0 +1,28 @@
+import sys
+
+si = sys.stdin.readline
+
+n,c=map(int,si().split())
+arr=[int(si()) for _ in range(n)]
+arr.sort()
+
+lo,hi= 1, arr[-1]-arr[0]
+result = 0
+
+while lo <= hi :
+    mid = (lo+hi)//2
+    count = 1
+    cur = arr[0]
+
+    for i in range(1, n) :
+        if arr[i]-cur >= mid :
+            cur = arr[i]
+            count += 1
+
+    if count >= c :
+        result = mid
+        lo = mid + 1
+    else :
+        hi = mid - 1
+
+print(result)
