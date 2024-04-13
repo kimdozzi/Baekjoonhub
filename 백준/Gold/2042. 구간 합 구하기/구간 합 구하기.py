5323ac41ef577a44da8cfb45f32cf213ad084e9c
DoHyung Kim <neo5188@gmail.com>
2024-04-14 00:55:00 +0900

    [Gold I] Title: 구간 합 구하기, Time: 628 ms, Memory: 139160 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -1,73 +1,45 @@
-# 0. 입력받기
-import sys
-input = sys.stdin.readline
-from math import ceil, log
+import sys, math
+si = sys.stdin.readline
 
-N, M, K = map(int,input().split())
+n,m,k=map(int, si().split())
+arr=[int(si()) for _ in range(n)]
+sz = math.ceil(math.log(n,2))
+MAX_ST = 2 ** sz
+seg_arr = [0 for _ in range(MAX_ST*2)]
 
-l = []
-segment_tree = [0]*(N*4)
+def construct_leaf() :
+    for i in range(MAX_ST, MAX_ST*2) :
+        if i-MAX_ST >= n :
+            seg_arr[i] = 0
+        else:
+            seg_arr[i] = arr[i-MAX_ST]
 
+def construct() :
+    for i in range(MAX_ST-1, 0, -1) :
+        seg_arr[i] = seg_arr[i*2] + seg_arr[i*2+1]
 
-# 1. 트리 만들기
-def init(start, end, index):
-	# start와 end가 같다면 리프노드이다.
-    if start == end:
-        segment_tree[index] = l[start-1]
-        return segment_tree[index]
-	
-    # 현재 노드는 왼쪽 아래 노드와 오른쪽 아래 노드를 더한 값이다.
-    mid = (start+end) // 2
-    segment_tree[index] = init(start, mid, index*2) + init(mid+1, end, index*2+1)
-    return segment_tree[index]
+def get_sum(L, R, nodeNum, curNodeL, curNodeR) :
+    if R < curNodeL or curNodeR < L : return False
+    if L <= curNodeL and curNodeR <= R : return seg_arr[nodeNum]
 
-       
-# 2. 트리에서 값 찾기
-def find(start, end, index, left, right):
-	# 찾으려는 범위가 start~end 범위보다 클 경우
-    if start > right or end < left:
-        return 0
-        
-    # 찾으려는 범위가 segment tree 노드안에 구현되어 있을 경우
-    if start >= left and end <= right:
-        return segment_tree[index]
+    mid = (curNodeL + curNodeR) // 2
+    return get_sum(L, R, nodeNum*2, curNodeL, mid) + get_sum(L, R, nodeNum*2+1, mid+1, curNodeR)
 
-	# 코드를 동작시키기 위한 기본 코드
-    # 현재 노드는 왼쪽아래 + 오른쪽아래 노드이다.
-    mid = (start + end) // 2
-    sub_sum = find(start, mid, index*2, left, right) + find(mid+1, end, index*2+1, left, right)
-    return sub_sum
+def update(idx, val) :
+    idx += MAX_ST
+    seg_arr[idx] = val
+    while idx > 1 :
+        idx //= 2
+        seg_arr[idx] = seg_arr[idx*2] + seg_arr[idx*2+1]
 
+construct_leaf()
+construct()
 
-# 3. 트리 값 바꿔주기
-def update(start, end, index, update_idx, update_data):
-	# update 하려는 범위가 초과될 경우
-    if start > update_idx or end < update_idx:
-        return
-    
-    segment_tree[index] += update_data
-	
-    # 리프노드까지 바꿔주었으면 재귀함수를 끝낸다.
-    if start == end:
-        return
-
-	# 내가 관여하고 있는 노드들을 찾아서 바꿔준다 -> 재귀함수로 구현
-    mid = (start + end) // 2
-    update(start, mid, index*2, update_idx, update_data)
-    update(mid+1, end, index*2+1, update_idx, update_data)
-
-
-for _ in range(N):
-    l.append(int(input()))
-
-init(1, N, 1)
-
-for _ in range(M+K):
-    a, b, c = map(int,input().split())
-    if a == 1:
-        temp = c - l[b-1]
-        l[b-1] = c
-        update(1, N, 1, b, temp)
-
-    elif a == 2:
-        print(find(1, N, 1, b, c))
+for _ in range(m+k) :
+    a,b,c=map(int,si().split())
+    if a == 1 :
+        # b -> c 로 변경
+        update(b-1, c)
+    elif a == 2 :
+        # [b,c] 까지 합 출력
+        print(get_sum(b-1,c-1,1,0,MAX_ST-1))
