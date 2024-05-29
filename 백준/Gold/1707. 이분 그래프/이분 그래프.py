71a3d5079110e067b2b6aec9c8fe8e780eb22057
DoHyung Kim <neo5188@gmail.com>
2024-05-29 11:20:26 +0900

    [Gold IV] Title: 이분 그래프, Time: 1184 ms, Memory: 53208 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,53 +1,37 @@
-import sys, heapq
-si = sys.stdin.readline
+import sys
 sys.setrecursionlimit(10**6)
-from collections import defaultdict, deque, Counter
-from bisect import bisect_left, bisect_right
-from math import factorial,sqrt
-from itertools import permutations, combinations, accumulate
-mn, mx = float('inf'), float('-inf')
-
-t=int(si())
+si = sys.stdin.readline
 
 
-def dfs(start,flag):
-    # 현재 정점을 flag(1)로 표시
+def dfs(start, flag) :
+    global graph
+
     visited[start] = flag
 
-    # 해당 정점과 인접한 정점을 순회
-    for x in graph[start] :
-
-        # 방문한 적이 없다면
-        if visited[x] == 0 :
-
-            # flag(-1 = 처음에 정한 1과 다른 팀) 표시변경 후 순회
-            res = dfs(x,-flag)
-
-            # 아래의 else를 통해 false를 반환받았다면 NO
-            if not res : return False
-
-        # 방문한 적이 있고
-        else :
-            # 현재 팀과 같은 팀이라면
-            if visited[x] == flag :
-                # NO
+    for i in graph[start] :
+        if visited[i] == 0 :
+            ans = dfs(i, -flag)
+            if not ans :
                 return False
-
-    # 모든 순회가 끝나면
+        else :
+            if visited[start] == visited[i] :
+                return False
     return True
 
+T = int(si())
 
-for _ in range(t) :
-    v,e=map(int,si().split())
+for _ in range(T) :
+    v, e = map(int,si().split())
     graph = [[] for _ in range(v+1)]
-    visited = [0 for _ in range(v+1)]
-    for i in range(e) :
-        edge, node = map(int,si().split())
-        graph[edge].append(node)
-        graph[node].append(edge)
-    for i in range(1,v+1) :
-        if visited[i] == 0 :
-            ans = dfs(i,1)
-            if not ans : break
-    print("YES") if ans else print("NO")
+    visited = [0] * (v+1)
+    for _ in range(e) :
+        a,b=map(int,si().split())
+        graph[a].append(b)
+        graph[b].append(a)
 
+    for i in range(1,v+1) :
+        if visited[i] == 0:
+            result = dfs(i, 1)
+            if not result:
+                break
+    print("YES") if result else print("NO")
