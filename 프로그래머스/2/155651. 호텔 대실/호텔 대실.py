059d016bffedef1f8d6c9b25b3f79310e8f70250
DoHyung Kim <neo5188@gmail.com>
2024-06-08 11:28:26 +0900

    Update 호텔 대실.py

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -15,3 +15,26 @@ def solution(book_time):
     
     return max(prefix_sum)
 
+# from collections import defaultdict
+
+# def to_time(val) :
+#     h, m = map(int,val.split(":"))
+#     return h * 60 + m
+    
+# def solution(book_time):
+#     book = defaultdict(int)
+#     for i in range(len(book_time)) :
+#         s,e = book_time[i]
+#         book[to_time(s)] += 1
+#         book[to_time(e)+10] += -1
+    
+#     book = sorted(book.items(), key=lambda x : x[0])
+    
+#     mx = float('-inf')
+#     cur = 0
+#     for b in book : 
+#         cur += b[1]
+#         mx = max(mx, cur)
+    
+#     return mx
+
