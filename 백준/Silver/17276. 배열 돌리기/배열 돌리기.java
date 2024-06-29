e4adfbc3f16e9aba268f837c5b125747c18100a4
DoHyung Kim <neo5188@gmail.com>
2024-06-29 10:30:41 +0900

    [Silver II] Title: 배열 돌리기, Time: 804 ms, Memory: 124644 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.java
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.java
@@ -5,75 +5,59 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int T;
-    static int[][] board;
+    static int[][] board, tempBoard;
     static int N, D;
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         T = Integer.parseInt(br.readLine());
-        for (int tc = 0; tc < T; tc++) {
-            input();
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            D = Integer.parseInt(st.nextToken());
+            if (D < 0) {
+                D += 360;
+            }
+            D /= 45;
 
-            solve();
+            board = new int[N][N];
+            tempBoard = new int[N][N];
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    board[i][j] = Integer.parseInt(st.nextToken());
+                    tempBoard[i][j] = board[i][j];
+                }
+            }
+            while (D-- > 0) {
+                rotate();
+            }
+            print();
         }
     }
 
-    private static void solve() {
-        int[][] tempBoard = new int[N][N];
-        boardClone(tempBoard, board);
-
-        if (D < 0)
-            D += 360;
-
-        for (int i = 0; i < D / 45; i++) {
-
-            rotate(board, tempBoard);
-
-            boardClone(board, tempBoard);
+    private static void rotate() {
+        for (int i = 0; i < N; i++) {
+            tempBoard[i][N / 2] = board[i][i];
+            tempBoard[i][N - i - 1] = board[i][N / 2];
+            tempBoard[N / 2][N - i - 1] = board[i][N - i - 1];
+            tempBoard[N - i - 1][N - i - 1] = board[N / 2][N - i - 1];
         }
-        print();
-    }
-
-    private static void boardClone(int[][] board, int[][] tempBoard) {
         for (int i = 0; i < N; i++) {
             board[i] = tempBoard[i].clone();
         }
     }
 
-    private static void rotate(int[][] board, int[][] tempBoard) {
-        for (int i=0; i<N; i++) {
-            tempBoard[i][N/2] = board[i][i];
-            tempBoard[i][N-i-1] = board[i][N/2];
-            tempBoard[N/2][N-i-1] = board[i][N-i-1];
-            tempBoard[N-i-1][N-i-1] = board[N/2][N-i-1];
-        }
-    }
-
     private static void print() {
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                System.out.print(board[i][j] + " ");
+                sb.append(board[i][j]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
         }
-    }
-
-
-    private static void input() throws IOException {
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        D = Integer.parseInt(st.nextToken());
-
-        board = new int[N][N];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+        System.out.print(sb);
     }
 }
