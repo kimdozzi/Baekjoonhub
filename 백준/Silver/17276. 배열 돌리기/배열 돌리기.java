cb44a87e57ded004868906eb39ee3a7fe7227af4
DoHyung Kim <neo5188@gmail.com>
2024-06-29 10:13:16 +0900

    [Silver II] Title: 배열 돌리기, Time: 4716 ms, Memory: 236516 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.java
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.java
@@ -21,47 +21,33 @@ public class Main {
 
     private static void solve() {
         int[][] tempBoard = new int[N][N];
+        boardClone(tempBoard, board);
 
-        for (int i = 0; i < N; i++) {
-            tempBoard[i] = board[i].clone();
+        if (D < 0)
+            D += 360;
+
+        for (int i = 0; i < D / 45; i++) {
+
+            rotate(board, tempBoard);
+
+            boardClone(board, tempBoard);
         }
-
-        // 시계 방향
-        if (D > 0) {
-            for (int i = 0; i < D / 45; i++) {
-                // 주 대각선 -> 가운데 열
-                fromMainCrossToMediumCol(board, tempBoard);
-                // 가운데 열 -> 부 대각선
-                fromMediumColToSubCross(board, tempBoard);
-                // 부 대각선 -> 가운데 행
-                fromSubCrossToMediumRow(board, tempBoard);
-                // 가운데 행 -> 주 대각선
-                fromMediumRowToMainCross(board, tempBoard);
-
-                for (int j = 0; j < N; j++) {
-                    board[j] = tempBoard[j].clone();
-                }
-            }
-        }
-        // 반시계 방향
-        else {
-            for (int i = 0; i < (360 + D) / 45; i++) {
-                // 주 대각선 -> 가운데 열
-                fromMainCrossToMediumCol(board, tempBoard);
-                // 가운데 열 -> 부 대각선
-                fromMediumColToSubCross(board, tempBoard);
-                // 부 대각선 -> 가운데 행
-                fromSubCrossToMediumRow(board, tempBoard);
-                // 가운데 행 -> 주 대각선
-                fromMediumRowToMainCross(board, tempBoard);
-
-                for (int j = 0; j < N; j++) {
-                    board[j] = tempBoard[j].clone();
-                }
-            }
-        }
-
         print();
+    }
+
+    private static void boardClone(int[][] board, int[][] tempBoard) {
+        for (int i = 0; i < N; i++) {
+            board[i] = tempBoard[i].clone();
+        }
+    }
+
+    private static void rotate(int[][] board, int[][] tempBoard) {
+        for (int i=0; i<N; i++) {
+            tempBoard[i][N/2] = board[i][i];
+            tempBoard[i][N-i-1] = board[i][N/2];
+            tempBoard[N/2][N-i-1] = board[i][N-i-1];
+            tempBoard[N-i-1][N-i-1] = board[N/2][N-i-1];
+        }
     }
 
     private static void print() {
@@ -70,43 +56,6 @@ public class Main {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
-        }
-    }
-
-    // 주 대각선 -> 가운데 열
-    private static void fromMainCrossToMediumCol(int[][] board, int[][] tempBoard) {
-        for (int i = 0; i < N; i++) {
-            for (int j = i; j <= i; j++) {
-                tempBoard[i][((N + 1) / 2) - 1] = board[i][j];
-            }
-        }
-    }
-
-    // 가운데 열 -> 부 대각선
-    private static void fromMediumColToSubCross(int[][] board, int[][] tempBoard) {
-        for (int i = 0; i < N; i++) {
-            for (int j = i; j <= i; j++) {
-                tempBoard[i][N - 1 - i] = board[i][((N + 1) / 2) - 1];
-            }
-        }
-
-    }
-
-    // 부 대각선 -> 가운데 행
-    private static void fromSubCrossToMediumRow(int[][] board, int[][] tempBoard) {
-        for (int i = 0; i < N; i++) {
-            for (int j = i; j <= i; j++) {
-                tempBoard[((N + 1) / 2) - 1][N - i - 1] = board[i][N - i - 1];
-            }
-        }
-    }
-
-    // 가운데 행 -> 주 대각선
-    private static void fromMediumRowToMainCross(int[][] board, int[][] tempBoard) {
-        for (int i = 0; i < N; i++) {
-            for (int j = i; j <= i; j++) {
-                tempBoard[N - i - 1][N - j - 1] = board[((N + 1) / 2) - 1][N - i - 1];
-            }
         }
     }
 
