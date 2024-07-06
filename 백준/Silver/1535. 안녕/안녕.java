8b75cfd1c12c78c002b6b02d278e8b1eb2b1c830
DoHyung Kim <neo5188@gmail.com>
2024-07-06 13:56:20 +0900

    [Silver II] Title: 안녕, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/1535. 안녕/안녕.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, answer = Integer.MIN_VALUE, hp = 100;
+    static int[][] people;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        people = new int[N][2];
+        dp = new int[N][101];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            people[i][0] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            people[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            int life = people[i][0];
+            int happy = people[i][1];
+
+            for (int j = 0; j < 100; j++) {
+                if (i == 0) {
+                    if (j >= life) {
+                        dp[i][j] = happy;
+                    }
+                } else {
+                    if (j >= life) {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - life] + happy);
+                    } else {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                    answer = Math.max(answer, dp[i][j]);
+                }
+            }
+        }
+        System.out.println(dp[N - 1][99]);
+    }
+}
