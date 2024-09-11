221e6503f03e855440fc1e3f7675429557ac75ed
DoHyung Kim <neo5188@gmail.com>
2024-09-11 13:55:09 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 76.98 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/README.md
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/README.md
@@ -1,10 +1,10 @@
 # [level 3] 파괴되지 않은 건물 - 92344 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92344) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92344?language=java) 
 
 ### 성능 요약
 
-메모리: 132 MB, 시간: 670.51 ms
+메모리: 211 MB, 시간: 76.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 27일 11:36:25
+2024년 09월 11일 13:55:07
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int solution(int[][] board, int[][] skill) {
+        int n = board.length;
+        int m = board[0].length;
+        int[][] imos = new int[n + 1][m + 1];
+        
+        // 스킬 적용
+        for (int[] s : skill) {
+            int type = s[0], r1 = s[1], c1 = s[2], r2 = s[3], c2 = s[4], degree = s[5];
+            int value = type == 1 ? -degree : degree;
+            
+            imos[r1][c1] += value;
+            imos[r2 + 1][c1] -= value;
+            imos[r1][c2 + 1] -= value;
+            imos[r2 + 1][c2 + 1] += value;
+        }
+        
+        // 누적합 계산
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i > 0) imos[i][j] += imos[i - 1][j];
+                if (j > 0) imos[i][j] += imos[i][j - 1];
+                if (i > 0 && j > 0) imos[i][j] -= imos[i - 1][j - 1];
+                
+                board[i][j] += imos[i][j];
+            }
+        }
+        
+        // 파괴되지 않은 건물 수 계산
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] > 0) answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
