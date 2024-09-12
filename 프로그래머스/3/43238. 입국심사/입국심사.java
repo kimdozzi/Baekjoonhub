d2e000fa189f4665a3f927e2ede834bd73aca4b9
DoHyung Kim <neo5188@gmail.com>
2024-09-12 18:19:30 +0900

    [level 3] Title: 입국심사, Time: 59.74 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,7 +1,9 @@
 class Solution {
+    // https://20240228.tistory.com/96
     public long solution(int n, int[] times) {
         long lo = 0;
         long hi = Long.MAX_VALUE/100;
+        System.out.println(hi);
         while(lo + 1 < hi) {
             long mid = lo + ((hi-lo)/2);
             
