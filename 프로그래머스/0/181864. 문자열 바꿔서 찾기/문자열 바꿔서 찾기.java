6d98a7224ff7d85e91539ca836f2ad931bba3ac5
DoHyung Kim <neo5188@gmail.com>
2024-09-13 16:09:12 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.07 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String myString, String pat) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch : myString.toCharArray())
+            if (ch == 'A') sb.append('B');
+            else sb.append('A');
+        return sb.toString().contains(pat) ? 1 : 0;
+    }
+}
