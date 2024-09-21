c8dc337ab0596fccc3be19a3d370e86628ce9f49
DoHyung Kim <neo5188@gmail.com>
2024-09-21 17:48:50 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,15 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for(int i=0; i<myString.length(); i++) {
-            if (myString.charAt(i) == 'a') {
-                answer += "A";
-            } else if ('B' <= myString.charAt(i) && myString.charAt(i) <= 'Z') {
-                answer += String.valueOf(myString.charAt(i)).toLowerCase();
-            } else {
-                answer += String.valueOf(myString.charAt(i));
-            }
-        }
-        return answer;
+        return myString.toLowerCase().replace("a", "A");
     }
 }
