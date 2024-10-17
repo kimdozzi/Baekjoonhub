23363b18fa991853bcb471f50a831cc93ea10aa0
DoHyung Kim <neo5188@gmail.com>
2024-10-18 00:36:07 +0900

    [level 2] Title: 기능개발, Time: 6.00 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,34 +1,15 @@
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        // 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고, 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됨.
-        
-        int cnt = 0;
-        int ans = 0;
-        List<Integer> list = new ArrayList<>();
+        int[] dayOfend = new int[100];
+        int day = 0;
         for(int i=0; i<progresses.length; i++) {
-            int n = progresses[i];
-            if (cnt != 0) {
-                n += cnt * speeds[i];
-                if(n>=100) {
-                    ans++;
-                    continue;
-                } 
-                else {
-                    list.add(ans);
-                    ans = 0;
-                }
+            while(progresses[i] + (day*speeds[i]) < 100) {
+                day++;
             }
-            
-            while (n < 100) {
-                n += speeds[i];
-                cnt++;
-            }    
-            ans++;
+            dayOfend[day]++;
         }
-        if (ans > 0) list.add(ans);
-        
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
     }
 }
