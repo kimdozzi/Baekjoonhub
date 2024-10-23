85ca4a04a9fd64c88647b0a985a8dd099c51b6d6
DoHyung Kim <neo5188@gmail.com>
2024-10-24 02:19:08 +0900

    [level 2] Title: 가장 큰 수, Time: 2863.91 ms, Memory: 384 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        List<String> list = new ArrayList<>();
+        for(int n: numbers) {
+            list.add(String.valueOf(n));
+        }
+        
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String str1 = o1+o2;
+                String str2 = o2+o1;
+                return str2.compareTo(str1);
+            }
+        });
+        
+        String answer = "";
+        for(String s : list) {
+            answer += s;
+        }
+        
+        return (answer.startsWith("0")) ? "0" : answer;
+    }
+}
