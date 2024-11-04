e4c7b8cffa474066f7f3a485a627a316df0b5df8
DoHyung Kim <neo5188@gmail.com>
2024-11-04 20:50:20 +0900

    [level 1] Title: 시저 암호, Time: 2.20 ms, Memory: 66.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,28 +1,16 @@
 class Solution {
     public String solution(String s, int n) {
-        char[] chArr = s.toCharArray();
-        char[] ans = new char[chArr.length];
-        for(int i=0; i<chArr.length; i++) {
-            if (65<=chArr[i] && chArr[i]<=90) {
-                int num = chArr[i] + n;
-                if (num > 90) {
-                    ans[i] = (char)((int)(num/90) * 64 + (num%90));
-                } else {
-                    ans[i] = (char)num;
-                }
-            } 
-            else if (97<=chArr[i] && chArr[i]<=122) {
-                int num = chArr[i] + n;
-                if (num > 122) {
-                    ans[i] = (char)((int)(num/122) * 96 + (num%122));
-                } else {
-                    ans[i] = (char)num;
-                }
-            } 
-            else {
-                ans[i] = ' ';
+        StringBuilder answer = new StringBuilder();
+        
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                answer.append(c);
+                continue;
             }
+            
+            char base = Character.isUpperCase(c) ? 'A' : 'a';
+            answer.append((char) (base + (c - base + n) % 26));
         }
-        return String.valueOf(ans);
+        return answer.toString();
     }
 }
