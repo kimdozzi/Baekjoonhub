8b9d0c2068a22fa75ac8a911b8b14fe16c46b060
DoHyung Kim <neo5188@gmail.com>
2024-11-10 16:34:10 +0900

    [Silver I] Title: 기타 레슨, Time: 260 ms, Memory: 24324 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+        
+        int max = 0;
+        int sum = 0;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, arr[i]);
+            sum += arr[i];
+		}
+		
+		int lo = max - 1, hi = sum;
+		while (lo + 1 < hi) {
+			int mid = (lo + hi) / 2;
+
+			int cnt = 1;
+			int currentSum = 0;
+			for (int i = 0; i < arr.length; i++) {
+				if (currentSum + arr[i] > mid) {
+					cnt++;
+					currentSum = 0;
+				}
+				currentSum += arr[i];
+			}
+
+			if (cnt <= m) {
+				hi = mid;
+			} else {
+				lo = mid;
+			}
+		}
+		System.out.println(hi);
+	}
+}
